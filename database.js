5fe5c3e4778125cc9eada60a2bf4de3ab70de30f
Mjmcdad <majdmekdad2017@gmail.com>
2024-08-02 20:30:17 +0300

    sequelize bug workaround

--- database.js
+++ database.js
@@ -13,8 +13,8 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process
 
   (async () => {
     try {
-      await sequelize.sync({force:true});
-      //await sequelize.authenticate();
+      //await sequelize.sync({force:true});
+      await sequelize.authenticate();
       console.log('Connection has been established successfully.');
     } catch (error) {
       console.error('Unable to connect to the database:', error);
